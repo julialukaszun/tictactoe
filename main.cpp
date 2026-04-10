241980a1e29dd7a42022e929bed89752708c4de3
mistressmyst <julialukaszun122@gmail.com>
2026-04-10 18:19:19 -0400

    configured raylib

--- main.cpp
+++ main.cpp
@@ -1,6 +1,18 @@
-#include <iostream>
+#include "raylib.h"
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+int main(void)
+{
+    InitWindow(800, 450, "raylib example - basic window");
+
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+        DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+        EndDrawing();
+    }
+
+    CloseWindow();
+
     return 0;
 }
