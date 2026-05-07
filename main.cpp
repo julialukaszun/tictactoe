ea314120be477fdca77b3548c0a49545760839b2
mistressmyst <julialukaszun122@gmail.com>
2026-05-06 21:53:54 -0400

    configured raylib

--- main.cpp
+++ main.cpp
@@ -1,14 +1,15 @@
 #include "raylib.h"
 
-int main(void)
+int main()
 {
-    InitWindow(800, 450, "raylib example - basic window");
+    InitWindow(800, 450, "Tic Tac Toe");
 
     while (!WindowShouldClose())
     {
         BeginDrawing();
         ClearBackground(RAYWHITE);
         DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+        DrawCircle(50, 50, 50, RED);
         EndDrawing();
     }
 
